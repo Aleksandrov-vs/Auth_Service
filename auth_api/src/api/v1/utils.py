9b0012ec882172d43c77e9299183469c5f672bde
vasya <dfcz200115@mail.ru>
2023-06-07 23:50:26 +0600

    фикс декоратора

--- auth_api/src/api/v1/utils.py
+++ auth_api/src/api/v1/utils.py
@@ -1,20 +1,36 @@
+import json
 from functools import wraps
 from typing import Type
 
 from pydantic import BaseModel
 from pydantic import ValidationError
-from flask import jsonify
+from flask import jsonify, request
 
 
 def body_fields_validate_with_pydantic(request_schema: Type[BaseModel]):
-    def decorator(funk):
-        @wraps(funk)
+    def decorator(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                res = funk(*args, **kwargs)
+                body = json.loads(request.data)
+            except json.JSONDecodeError:
+                return jsonify({'err_msg': 'json is not valid'}), 400
+            try:
+                body = request_schema(**body)
+                res = func(body, *args, **kwargs)
             except ValidationError as e:
-                return jsonify(str(e)), 400
+                return jsonify({'err_msg': str(e)}), 400
             else:
                 return res
         return wrapper
     return decorator
+
+
+def request_has_user_agent(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not request.headers.get('User-Agent'):
+            return jsonify({'err_msg': 'User-Agent is require.'}), 400
+        res = func(*args, **kwargs)
+        return res
+    return wrapper
