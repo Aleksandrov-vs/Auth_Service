88939b281d5a1ac76f3f77a212f486d53c69b55c
Mikhail Derunov <mederunov@gmail.com>
2023-06-08 17:29:44 +0300

    feat: Написана бизнес-логика для работы с Users

---
+++ auth_api/src/services/user_service.py
@@ -0,0 +1,41 @@
+import logging
+from functools import lru_cache
+from uuid import UUID
+
+from src.models.user import User
+from src.repositories.user_rep import UserRepository
+
+
+class UserService:
+    def __init__(self, user_rep: UserRepository):
+        self._repository = user_rep
+
+    def get_all_users(self):
+        users = self._repository.get_all_users_with_id()
+        return users
+
+    def get_user(self, login: str) -> User:
+        role = self._repository.get_user_by_login(login)
+        return role
+
+    def assign_role(self, user_id: UUID, role_id: UUID):
+        result = self._repository.assign_role_to_user(user_id, role_id)
+        return result
+
+    def revoke_role(self, user_id: UUID, role_id: UUID):
+        role = self._repository.revoke_role_from_user(user_id, role_id)
+        return role
+
+    def viewing_role(self, user_id: UUID) -> list[str] | None:
+        role = self._repository.get_user_permissions(user_id=user_id)
+        return role
+
+    def user_history(self, user_id: UUID) -> list[str] | None:
+        history = self._repository.get_user_history(user_id=user_id)
+        return history
+
+
+@lru_cache()
+def get_user_service(user_repository: UserRepository):
+    logging.info('init UserServices')
+    return UserService(user_repository)
