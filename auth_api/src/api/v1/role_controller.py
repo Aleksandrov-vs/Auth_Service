f8d57f087f4373d299316dc5bd29a6d05fcff6e4
vasya <dfcz200115@mail.ru>
2023-06-08 15:59:16 +0600

    fix(role permission): ограничение к СRUD ролей

--- auth_api/src/api/v1/role_controller.py
+++ auth_api/src/api/v1/role_controller.py
@@ -1,11 +1,12 @@
 import json
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required
 from pydantic import BaseModel
 
 from src.services.role_service import get_role_service
 from src.repositories import role_rep
-from .utils import validator_json_request
+from .utils import validator_json_request, check_user_has_role
 
 
 role_bp = Blueprint('role', __name__, url_prefix='/api/v1/auth')
@@ -21,6 +22,8 @@ class ValidatorJsonUpdate(BaseModel):
 
 
 @role_bp.route('/roles', methods=['POST'])
+@jwt_required()
+@check_user_has_role('admin')
 @validator_json_request(ValidatorJsonPostDelete)
 def role_create(body: ValidatorJsonPostDelete):
     role_service = get_role_service(role_rep.get_role_repository())
@@ -29,6 +32,8 @@ def role_create(body: ValidatorJsonPostDelete):
 
 
 @role_bp.route('/roles/delete', methods=['DELETE'])
+@jwt_required()
+@check_user_has_role('admin')
 @validator_json_request(ValidatorJsonPostDelete)
 def role_delete(body: ValidatorJsonPostDelete):
     role_service = get_role_service(role_rep.get_role_repository())
@@ -37,6 +42,8 @@ def role_delete(body: ValidatorJsonPostDelete):
 
 
 @role_bp.route('/roles/update', methods=['PUT'])
+@jwt_required()
+@check_user_has_role('admin')
 @validator_json_request(ValidatorJsonUpdate)
 def role_update(body: ValidatorJsonUpdate):
     role_service = get_role_service(role_rep.get_role_repository())
@@ -45,6 +52,8 @@ def role_update(body: ValidatorJsonUpdate):
 
 
 @role_bp.route('/roles/view', methods=['GET'])
+@jwt_required()
+@check_user_has_role('admin')
 def roles_viewed():
     role_service = get_role_service(role_rep.get_role_repository())
     response = role_service.viewing_roles()
