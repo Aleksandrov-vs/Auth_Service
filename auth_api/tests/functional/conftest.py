6e326ca18ccef933e99ec6f7220f2fa750a4bc7e
vasya <dfcz200115@mail.ru>
2023-06-08 22:13:01 +0600

    вынес работу с постгрес в другой файл

--- auth_api/tests/functional/conftest.py
+++ auth_api/tests/functional/conftest.py
@@ -1,51 +1,16 @@
 import asyncio
-import json
 
 import aiohttp
 import pytest
 import pytest_asyncio
-import sqlalchemy
 from redis.asyncio import Redis
-from sqlalchemy import create_engine, text
 
 from functional.settings import test_settings
 
 
-@pytest.fixture(scope='session')
-def pg_engine():
-    engine = create_engine(f"postgresql://{test_settings.pg_user}:"
-                           f"{test_settings.pg_password}@{test_settings.pg_host}"
-                           f":{test_settings.pg_port}/{test_settings.dbname}")
-    yield engine
-
-
-@pytest.fixture(scope="session", autouse=True)
-def delete_all_pg_data(pg_engine: sqlalchemy.Engine):
-    def delete(engine):
-        table_names = [
-            "auth_history",
-            "user_role",
-            "roles",
-            "users",
-        ]
-
-        with engine.begin() as connection:
-            for table in table_names:
-                query = text(f"TRUNCATE {table} RESTART IDENTITY CASCADE;")
-                connection.execute(
-                    query
-                )
-
-    delete(pg_engine)
-    yield
-    delete(pg_engine)
-
-
-# @pytest.fixture(scope="session", autouse=True)
-# def super_user(session_client: aiohttp.ClientSession, pg_engine: sqlalchemy.Engine):
-#
-#     pass
-
+pytest_plugins = [
+   "functional.fixtures.create_pg_data",
+]
 
 
 @pytest.fixture(scope="session")
@@ -56,7 +21,7 @@ def event_loop():
     loop.close()
 
 
-@pytest_asyncio.fixture(scope='function')
+@pytest_asyncio.fixture(scope='session')
 async def session_client():
     session = aiohttp.ClientSession()
     yield session
@@ -70,7 +35,7 @@ async def redis_client():
     await client.close()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session')
 def make_get_request(session_client: aiohttp.ClientSession):
     async def inner(
             url: str, params: dict = None,
@@ -89,16 +54,14 @@ def make_get_request(session_client: aiohttp.ClientSession):
                 return body, status
 
         if method == 'DELETE':
-            async with session_client.delete(url, json=data) as response:
+            async with session_client.delete(url, json=data, headers=headers) as response:
                 body = await response.json()
                 status = response.status
                 return body, status
 
         if method == 'PUT':
-            async with session_client.put(url, json=data) as response:
+            async with session_client.put(url, json=data, headers=headers) as response:
                 body = await response.json()
                 status = response.status
                 return body, status
-
-
     return inner
