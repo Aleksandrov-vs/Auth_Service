99dd57b7f2b8a380ec5d6bbda9ebb6d22e3acacd
Mikhail Derunov <mederunov@gmail.com>
2023-06-09 21:01:07 +0300

    feat: Написаны тесты для endpoint'ов Users

--- auth_api/tests/functional/src/test_user.py
+++ auth_api/tests/functional/src/test_user.py
@@ -1,8 +1,11 @@
+import logging
 import os
 import sys
+import uuid
 from http import HTTPStatus
 
 import pytest
+from functional.testdata.pg_data import ADMIN_USER
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -16,58 +19,26 @@ pytestmark = pytest.mark.asyncio
     'query_data, expected_answer, expected_status',
     [
         (
-            {'login': 'not_exist',
-             'reg_request_body': {
-                 "login": "login",
-                 "password": "pass"
-             }
-             },
-            {"message": "User not found."}, HTTPStatus.OK
+                {'login': 'not_exist'},
+                {"message": "User not found."},
+                HTTPStatus.OK
         ),
-(
-            {'login': 'login_valid',
-             'reg_request_body': {
-                 "login": "login_valid",
-                 "password": "pass"
-             }
-             },
-            {"login": "login_valid"}, HTTPStatus.OK
+        (
+                {'login': 'super_user'},
+                {"login": "super_user"},
+                HTTPStatus.OK
         )
     ]
 )
-async def test_user_find(make_get_request, delete_all_pg_data, query_data, expected_answer, expected_status):
-    # регистрация пользователя
-    _, reg_status = await make_get_request(
-        test_settings.service_url + '/api/v1/auth/register',
-        method='POST',
-        data=query_data['reg_request_body']
-    )
-    assert reg_status == HTTPStatus.OK, 'ошибка регистрации'
-
-    # логин
-    body, status = await make_get_request(
-        test_settings.service_url + '/api/v1/auth/login',
-        method='POST',
-        data=query_data['reg_request_body']
-    )
-
-    if status == HTTPStatus.OK:
-        assert 'access_token' in body
-        assert 'refresh_token' in body
-
-    token = body['access_token']
-
-    if not query_data.get('headers'):
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
-
+async def test_user_find(make_get_request, admin_tokens, query_data, expected_answer, expected_status):
+    tokens = admin_tokens
     url = test_settings.service_url + f'/api/v1/auth/users/{query_data["login"]}'
-
     body, status = await make_get_request(
         url,
         method='GET',
-        headers=headers
+        headers={
+            "Authorization": f"Bearer {tokens.get('access_token')}"
+        }
     )
 
     if 'User not found.' != body.get('message'):
@@ -77,129 +48,117 @@ async def test_user_find(make_get_request, delete_all_pg_data, query_data, expec
     assert status == expected_status
 
 
+@pytest.mark.parametrize(
+    'query_data, expected_answer, expected_status',
+    [
+        (
+                {
+                    'role_create': {'name': 'some_role'},
+                    'user_id': ADMIN_USER['id'],
+                    'request': {'role_name': 'another_role'}
+                },
+                {'role_assign': {'message': 'User or role not found.'},
+                 'check_role': ['admin'],
+                 'role_revoke': {'message': 'User or role not found.'}},
+                HTTPStatus.OK
+        ),
+        (
+                {
+                    'role_create': {'name': 'test_role'},
+                    'user_id': ADMIN_USER['id'],
+                    'request': {"role_name": "test_role"}
+                },
+                {'role_assign': {'message': 'Role assigned to user successfully.'},
+                 'check_role': ['admin', 'test_role'],
+                 'role_revoke': {'message': 'Role revoked from user successfully.'}},
+                HTTPStatus.OK
+        ),
+    ]
+)
+async def test_assign_revoke_role(make_get_request, delete_all_pg_data, admin_tokens, query_data, expected_answer,
+                                  expected_status):
+    url = test_settings.service_url + '/api/v1/auth/roles'
 
-# @pytest.mark.parametrize(
-#     'query_data, expected_answer, expected_status',
-#     [
-#         (
-#             {'name': 'admin', 'new_name': 'admin'}, {'message': 'Role already exists.'}, HTTPStatus.OK
-#         )
-#     ]
-# )
-# async def test_role_update_exists(make_get_request, query_data, expected_answer, expected_status):
-#     url = test_settings.service_url + '/api/v1/auth/roles/update'
-#
-#     body, status = await make_get_request(
-#         url,
-#         method='PUT',
-#         data=query_data
-#     )
-#
-#     assert body == expected_answer
-#     assert status == expected_status
-#
-#
-# @pytest.mark.parametrize(
-#     'query_data, expected_answer, expected_status',
-#     [
-#         (
-#             {'name': 'admin'}, {'message': 'Role already exists.'}, HTTPStatus.OK
-#         )
-#     ]
-# )
-# async def test_role_create_replay(make_get_request, query_data, expected_answer, expected_status):
-#     url = test_settings.service_url + '/api/v1/auth/roles'
-#
-#     body, status = await make_get_request(
-#         url,
-#         method='POST',
-#         data=query_data
-#     )
-#
-#     assert body == expected_answer
-#     assert status == expected_status
-#
-#
-# @pytest.mark.parametrize(
-#     'query_data, expected_answer, expected_status',
-#     [
-#         (
-#             {'name': 'admin', 'new_name': 'like a boss'}, 'like a boss', HTTPStatus.OK
-#         )
-#     ]
-# )
-# async def test_role_update(make_get_request, query_data, expected_answer, expected_status):
-#     url = test_settings.service_url + '/api/v1/auth/roles/update'
-#
-#     body, status = await make_get_request(
-#         url,
-#         method='PUT',
-#         data=query_data
-#     )
-#
-#     assert body['name'] == expected_answer
-#     assert status == expected_status
-#
-#
-# @pytest.mark.parametrize(
-#     'expected_answer, expected_status',
-#     [
-#         (
-#             'like a boss', HTTPStatus.OK
-#         )
-#     ]
-# )
-# async def test_role_viewing(make_get_request, expected_answer, expected_status):
-#     url = test_settings.service_url + '/api/v1/auth/roles/view'
-#
-#     body, status = await make_get_request(
-#         url,
-#         method='GET'
-#     )
-#
-#     assert body['name'] == expected_answer
-#     assert status == expected_status
-#
-#
-# @pytest.mark.parametrize(
-#     'query_data, expected_answer, expected_status',
-#     [
-#         (
-#             {'name': 'like a boss'}, {'message': 'Role successfully deleted.'}, HTTPStatus.OK
-#         ),
-#         (
-#             {'name': 'admin'}, {'message': 'Role does not exists.'}, HTTPStatus.OK
-#         )
-#     ]
-# )
-# async def test_role_delete(make_get_request, query_data, expected_answer, expected_status):
-#     url = test_settings.service_url + '/api/v1/auth/roles/delete'
-#
-#     body, status = await make_get_request(
-#         url,
-#         method='DELETE',
-#         data=query_data
-#     )
-#
-#     assert body == expected_answer
-#     assert status == expected_status
-#
-#
-# @pytest.mark.parametrize(
-#     'expected_answer, expected_status',
-#     [
-#         (
-#             {'message': 'No roles was found.'}, HTTPStatus.OK
-#         )
-#     ]
-# )
-# async def test_role_viewing_reply(make_get_request, expected_answer, expected_status):
-#     url = test_settings.service_url + '/api/v1/auth/roles/view'
-#
-#     body, status = await make_get_request(
-#         url,
-#         method='GET'
-#     )
-#
-#     assert body == expected_answer
-#     assert status == expected_status
+    tokens = admin_tokens
+    body, status = await make_get_request(
+        url,
+        method='POST',
+        data=query_data['role_create'],
+        headers={
+            "Authorization": f"Bearer {tokens.get('access_token')}"
+        }
+    )
+    assert status == HTTPStatus.OK
+    logging.info('Role is created')
+    # test creating role
+    url = test_settings.service_url + f'/api/v1/auth/users/{query_data["user_id"]}/assign-role'
+    logging.info(query_data['request'])
+    body, status = await make_get_request(
+        url,
+        method='POST',
+        data=query_data['request'],
+        headers={
+            "Authorization": f"Bearer {tokens.get('access_token')}"
+        }
+    )
+    assert body == expected_answer['role_assign']
+    assert status == expected_status
+
+    # test check-permission
+    url = test_settings.service_url + f'/api/v1/auth/users/{query_data["user_id"]}/check-permissions'
+    body, status = await make_get_request(
+        url,
+        method='GET',
+        headers={
+            "Authorization": f"Bearer {tokens.get('access_token')}"
+        }
+    )
+    assert expected_answer['check_role'].sort() == body['permissions'].sort()
+    assert status == expected_status
+
+    # revoke role
+    url = test_settings.service_url + f'/api/v1/auth/users/{query_data["user_id"]}/revoke-role'
+    logging.info(query_data['request'])
+    body, status = await make_get_request(
+        url,
+        method='POST',
+        data=query_data['request'],
+        headers={
+            "Authorization": f"Bearer {tokens.get('access_token')}"
+        }
+    )
+    assert body == expected_answer['role_revoke']
+    assert status == expected_status
+
+
+@pytest.mark.parametrize(
+    'query_data, expected_answer, expected_status',
+    [
+        (
+                {'user_id': ADMIN_USER['id']},
+                {"message": "User not found."},
+                HTTPStatus.OK
+        ),
+        (
+                {'user_id': uuid.uuid4()},
+                {'message': 'User history is not found'},
+                HTTPStatus.OK
+        )
+    ]
+)
+async def test_user_history(make_get_request, admin_tokens, query_data, expected_answer, expected_status):
+    tokens = admin_tokens
+    url = test_settings.service_url + f'/api/v1/auth/users/{query_data["user_id"]}/history'
+    body, status = await make_get_request(
+        url,
+        method='GET',
+        headers={
+            "Authorization": f"Bearer {tokens.get('access_token')}"
+        }
+    )
+    assert status == expected_status
+    if type(body) == dict:
+        assert body == expected_answer
+    else:
+        for row in body:
+            assert row['user_id'] == str(query_data["user_id"])
