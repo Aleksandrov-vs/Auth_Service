a094d390d6984b4240401a76404e4343e5d72abd
vasya <dfcz200115@mail.ru>
2023-06-10 03:17:49 +0600

    fix(perm): доступ к эндпоинтам users теперь только у админов

--- auth_api/src/api/v1/user_controller.py
+++ auth_api/src/api/v1/user_controller.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required
 from pydantic import BaseModel
+
 from src.repositories import user_rep
 from src.services.user_service import get_user_service
 
-from .utils import validator_json_request
+from .utils import validator_json_request, check_user_has_role
 
 user_bp = Blueprint('user', __name__, url_prefix='/api/v1/auth')
 
@@ -13,6 +15,8 @@ class RoleValidator(BaseModel):
 
 
 @user_bp.route('/users/<login>', methods=['GET'])
+@jwt_required()
+@check_user_has_role('admin')
 def get_user(login):
     user_service = get_user_service(user_rep.get_user_repository())
     response = user_service.get_user(login=login)
@@ -20,6 +24,8 @@ def get_user(login):
 
 
 @user_bp.route('/users/<user_id>/assign-role', methods=['POST'])
+@jwt_required()
+@check_user_has_role('admin')
 @validator_json_request(RoleValidator)
 def assign_role(body: RoleValidator, user_id):
     user_service = get_user_service(user_rep.get_user_repository())
@@ -29,6 +35,8 @@ def assign_role(body: RoleValidator, user_id):
 
 
 @user_bp.route('/users/<user_id>/revoke-role', methods=['POST'])
+@jwt_required()
+@check_user_has_role('admin')
 @validator_json_request(RoleValidator)
 def revoke_role(body: RoleValidator, user_id):
     user_service = get_user_service(user_rep.get_user_repository())
@@ -38,6 +46,8 @@ def revoke_role(body: RoleValidator, user_id):
 
 
 @user_bp.route('/users/<user_id>/check-permissions', methods=['GET'])
+@jwt_required()
+@check_user_has_role('admin')
 def check_role(user_id):
     user_service = get_user_service(user_rep.get_user_repository())
     roles = user_service.viewing_role(user_id=user_id)
@@ -45,6 +55,8 @@ def check_role(user_id):
 
 
 @user_bp.route('/users/<user_id>/history', methods=['GET'])
+@jwt_required()
+@check_user_has_role('admin')
 def user_history(user_id):
     user_service = get_user_service(user_rep.get_user_repository())
     history = user_service.user_history(user_id=user_id)
