6599f745357ff20e89b35f103d402a56680ca441
Sergey Toporets <yateperna@yandex.ru>
2023-06-13 12:26:59 +0300

    Написал tracer

---
+++ auth_api/src/utils/trace_func.py
@@ -0,0 +1,22 @@
+from functools import wraps
+import asyncio
+
+from opentelemetry import trace
+
+
+def traced(name: str = None):
+    def wrapper(func):
+        if not asyncio.iscoroutinefunction(func):
+            @wraps(func)
+            def inner(*args, **kwargs):
+                tracer = trace.get_tracer(__name__)
+                with tracer.start_as_current_span(func.__name__ if not name else name):
+                    return func(*args, **kwargs)
+        else:
+            @wraps(func)
+            async def inner(*args, **kwargs):
+                tracer = trace.get_tracer(__name__)
+                with tracer.start_as_current_span(func.__name__ if not name else name):
+                    return await func(*args, **kwargs)
+        return inner
+    return wrapper
---
+++ auth_api/src/utils/tracer.py
@@ -0,0 +1,31 @@
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (BatchSpanProcessor,
+                                            ConsoleSpanExporter)
+from src.core.config import settings
+
+
+def configure_tracer() -> None:
+    trace.set_tracer_provider(
+        TracerProvider(
+            resource=Resource.create(
+                attributes={
+                    "service.name": "auth-api",
+                    "service.namespace": "AuthApi",
+                    "service.version": "1.0.0",
+                }
+            )
+        )
+    )
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                agent_host_name=settings.tracer_host,
+                agent_port=settings.tracer_port,
+            )
+        )
+    )
+    if settings.console_tracer_enabled:
+        trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
