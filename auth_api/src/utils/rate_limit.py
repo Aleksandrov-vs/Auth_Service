ad9fc49273058a55761bc10db937960dc9a99cac
Sergey Toporets <yateperna@yandex.ru>
2023-06-16 18:10:02 +0300

    Сделал возможность отключения лимитера

--- auth_api/src/utils/rate_limit.py
+++ auth_api/src/utils/rate_limit.py
@@ -15,20 +15,20 @@ redis_rate_limit = redis.StrictRedis(
     decode_responses=True
 )
 
-
-def rate_limit():
-    def wrapper(func):
-        @wraps(func)
-        def inner(*args, **kwargs):
-            pipline = redis_rate_limit.pipeline()
-            now = datetime.now()
-            key = f"{request.remote_addr}:{now.minute}"
-            pipline.incr(key, 1)
-            pipline.expire(key, 59)
-            request_number = pipline.execute()[0]
-            if request_number > settings.default_rate_limit:
-                return jsonify(
-                    msg="Too many requests"), HTTPStatus.TOO_MANY_REQUESTS
-            return func(*args, **kwargs)
-        return inner
-    return wrapper
+if settings.rate_limit_enabled:
+    def rate_limit():
+        def wrapper(func):
+            @wraps(func)
+            def inner(*args, **kwargs):
+                pipline = redis_rate_limit.pipeline()
+                now = datetime.now()
+                key = f"{request.remote_addr}:{now.minute}"
+                pipline.incr(key, 1)
+                pipline.expire(key, 59)
+                request_number = pipline.execute()[0]
+                if request_number > settings.default_rate_limit:
+                    return jsonify(
+                        msg="Too many requests"), HTTPStatus.TOO_MANY_REQUESTS
+                return func(*args, **kwargs)
+            return inner
+        return wrapper
